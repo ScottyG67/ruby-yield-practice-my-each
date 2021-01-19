5545f20811b4782069de51886a82620386be40e7
github username <scott.gloyna01@gmail.com>
2021-01-19 10:38:53 -0600

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
+require 'pry'
+def my_each(collection) # put argument(s) here
   # code here
+  i = 0
+  return_array = []
+
+  while i < collection.length
+    yield(collection[i])
+    return_array << collection[i]
+    i+=1
+  end
+  return_array
 end
+
